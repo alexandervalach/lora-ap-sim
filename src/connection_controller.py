6679f741fd0016f164c1e8ee919b356f459862c4
Alexander Valach <alexander.valach@gmail.com>
2020-03-23 18:35:41 +0100

    SSL connection controller

---
+++ src/connection_controller.py
@@ -0,0 +1,29 @@
+import socket
+import ssl
+
+
+class ConnectionController:
+    def __init__(self, host, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+
+        self.conn = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1)
+        self.host = host
+        self.port = port
+
+    def connect(self):
+        try:
+            self.conn.connect((self.host, self.port))
+        except InterruptedError as e:
+            print("An error occurred during a connection attempt")
+            print(e)
+        # finally:
+        # self.conn.close()
+
+    def send_data(self, data):
+        self.conn.send(data)
+        reply = self.conn.recv(1280)
+        print(reply)
+
+    def close(self):
+        self.conn.close()
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from connection_controller import ConnectionController
+
+data = '{"message_name": "SETR","message_body": {"id": "11111111","ver": 1.0,"m_chan": true,"channels": 8,"sup_freqs": [863000000,100000,870000000],"sup_sfs": [7,8,9,10,11,12],"sup_crs": ["4/5","4/6","4/7","4/8"],"sup_bands": [500000,250000,125000],"lora_stand": {"name": "Lora@FIIT","version": "1.0"},"max_power": 14}}'
+host = '147.175.149.229'
+port = 25001
+
+conn = ConnectionController(host, port)
+conn.connect()
+conn.send_data(data)
