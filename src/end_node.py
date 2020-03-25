23ca023c7e0904adf9bf2c967e593cda9bb140a3
Alexander Valach <alexander.valach@gmail.com>
2020-03-25 20:01:00 +0100

    End node message generation

---
+++ src/end_node.py
@@ -0,0 +1,51 @@
+import json
+
+class EndNode:
+    def generate_regr(self):
+        message = {}
+        message_body = {}
+
+        message['message_name'] = 'REGR'
+        message_body['band'] = 125000
+        message_body['cr'] = "4/5"
+        message_body['dev_id'] = "ALEX"
+        message_body['power'] = 14
+        message_body['duty_c'] = 36000
+        message_body['rssi'] = -57.0
+        message_body['sf'] = 9
+        message_body['snr'] = 10.75
+        message_body['time'] = 15752
+        message_body['app_data'] = 'TE9SQUZJSVQ='
+        message_body['sh_key'] = '+/////v////7////+////wIAAAA='
+        message['message_body'] = message_body
+
+        json_message = json.dumps(message, separators=(',', ':'))
+        print(json_message)
+
+        return json_message
+
+    def generate_rxl(self, dev_id, seq):
+        message = {}
+        message_body = {}
+
+        message['message_name'] = 'RXL'
+        message_body['seq'] = seq
+        message_body['band'] = 125000
+        message_body['cr'] = "4/5"
+        message_body['dev_id'] = dev_id
+        message_body['power'] = 12
+        message_body['duty_c'] = 36000
+        message_body['rssi'] = -67.0
+        message_body['sf'] = 7
+        message_body['snr'] = 12.75
+        message_body['time'] = 10024
+        message_body['conf_need'] = False
+        # UNSUPPORTED, VOLATILE, MANDATORY
+        message_body['ack'] = 'UNSUPPORTED'
+        message_body['data'] = 'TE9SQUZJSVQ='
+        message['message_body'] = message_body
+
+        json_message = json.dumps(message, separators=(',', ':'))
+        print(json_message)
+
+        return json_message
--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 from connection_controller import ConnectionController
+from end_node import EndNode
 import time
 
-setrMessage = '{"message_name": "SETR","message_body": {"id": "11111111","ver": 1.0,"m_chan": true,"channels": 8,"sup_freqs": [863000000,100000,870000000],"sup_sfs": [7,8,9,10,11,12],"sup_crs": ["4/5","4/6","4/7","4/8"],"sup_bands": [500000,250000,125000],"lora_stand": {"name": "Lora@FIIT","version": "1.0"},"max_power": 14}}'
-regrMessage = '{"message_name":"REGR", "message_body":{ "band": 125000, "cr": "4/5", "dev_id":"ALBY", "power":14, "duty_c": 36000, "rssi": -57.0, "sf": 9, "snr": 10.75, "time": 15752, "app_data":"TE9SQUZJSVQ=", "sh_key":"+/////v////7////+////wIAAAA=" }}'
+setr_message = '{"message_name": "SETR","message_body": {"id": "11111111","ver": 1.0,"m_chan": true,"channels": 8,"sup_freqs": [863000000,100000,870000000],"sup_sfs": [7,8,9,10,11,12],"sup_crs": ["4/5","4/6","4/7","4/8"],"sup_bands": [500000,250000,125000],"lora_stand": {"name": "Lora@FIIT","version": "1.0"},"max_power": 14}}'
 host = '147.175.149.229'
 port = 25001
 
 conn = ConnectionController(host, port)
 conn.connect()
-conn.send_data(setrMessage)
-# conn.send_data(regrMessage)
+conn.send_data(setr_message)
+end_node = EndNode()
+
+node_ids = ['yv4j', 'ALBY', 'QUFB', 'ALEX', 'Jaro']
+seq = 1
 
 while True:
-    conn.send_data(regrMessage)
+    for node_id in node_ids:
+        rxl_message = end_node.generate_rxl(node_id, seq)
+        conn.send_data(rxl_message)
+    seq += 1
     time.sleep(10)
