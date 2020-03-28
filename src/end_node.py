cfbd4c1fa0bb37483eda9456d7a062a7591fc471
Alexander Valach <alexander.valach@gmail.com>
2020-03-28 16:42:33 +0100

    Added random SNR and RSSI values

--- src/end_node.py
+++ src/end_node.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 class EndNode:
     def generate_regr(self):
@@ -11,16 +12,16 @@ class EndNode:
         message_body['dev_id'] = "ALEX"
         message_body['power'] = 14
         message_body['duty_c'] = 36000
-        message_body['rssi'] = -57.0
+        message_body['rssi'] = self.rssi()
         message_body['sf'] = 9
-        message_body['snr'] = 10.75
+        message_body['snr'] = self.snr()
         message_body['time'] = 15752
         message_body['app_data'] = 'TE9SQUZJSVQ='
         message_body['sh_key'] = '+/////v////7////+////wIAAAA='
         message['message_body'] = message_body
 
         json_message = json.dumps(message, separators=(',', ':'))
-        print(json_message)
+        # print(json_message)
 
         return json_message
 
@@ -38,14 +39,22 @@ class EndNode:
         message_body['dev_id'] = dev_id
         message_body['duty_c'] = 36000
         # message_body['power'] = 12
-        message_body['rssi'] = -67.0
+        message_body['rssi'] = self.rssi()
         message_body['seq'] = seq
         message_body['sf'] = 7
-        message_body['snr'] = 12.75
+        message_body['snr'] = self.snr()
         message_body['time'] = 10024
         message['message_body'] = message_body
 
         json_message = json.dumps(message, separators=(',', ':'))
-        print(json_message)
+        # print(json_message)
 
         return json_message
+
+    """ Randomly generated SNR values between 6.5 and 11.75"""
+    def snr(self):
+        return round(random.uniform(0,1) * 5.25 + 6.5, 2)
+
+    """ Randomly generated RSSI values between -35 and -115"""
+    def rssi(self):
+        return round(random.uniform(0,1) * (-80) -35, 1)
